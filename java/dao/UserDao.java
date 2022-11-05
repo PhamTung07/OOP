15016c5de31ec0a561289e8854090d8f920bb24e
PhamTung07 <111500169+PhamTung07@users.noreply.github.com>
2022-11-05 10:33:25 +0700

    Add files via upload

---
+++ java/dao/UserDao.java
@@ -0,0 +1,15 @@
+package qlsv.dao;
+
+import qlsv.entity.User;
+
+public class UserDao {
+    public boolean checkUser(User user) {
+        if (user != null) {
+            if ("admin".equals(user.getUserName()) 
+                    && "admin".equals(user.getPassword())) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
